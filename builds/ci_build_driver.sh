57fca17e0cf96f3368e2a109b0dc09d46bc014e1
yuchengt900 <yi-cheng.teng@noaa.gov>
2024-04-18 08:10:18 -0400

    Add FMS1 build option

--- builds/ci_build_driver.sh
+++ builds/ci_build_driver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=60
-#SBATCH --job-name="MOM6SIS2_build"
-#SBATCH --output=MOM6SIS2_build_o.%j
-#SBATCH --error=MOM6SIS2_build_e.%j
+#SBATCH --job-name="MOM6SIS2_ci_build"
+#SBATCH --output=MOM6SIS2_ci_build_o.%j
+#SBATCH --error=MOM6SIS2_ci_build_e.%j
 #SBATCH --qos=debug
 #SBATCH --partition=batch
 #SBATCH --clusters=c5
@@ -18,7 +18,7 @@
 echo "Build started:  " `date`
 
 #
-./linux-build.bash -m gaea -p ncrc5.intel23 -t repro -f mom6sis2
+./linux-build.bash -m gaea -p ncrc5.intel23 -t repro -f fms1_mom6sis2
 
 #
 echo "Build ended:  " `date`
--- builds/linux-build.bash
+++ builds/linux-build.bash
@@ -48,14 +48,6 @@ source $rootdir/$machine_name/$platform.env
 
 makeflags="NETCDF=3"
 
-if [[ "$target" =~ "openmp" ]] ; then 
-   makeflags="$makeflags OPENMP=1" 
-fi
-
-if [[ "$target" =~ "openacc" ]] ; then 
-   makeflags="$makeflags OPENACC=1" 
-fi
-
 if [[ $target =~ "repro" ]] ; then
    makeflags="$makeflags REPRO=1"
 fi
@@ -80,7 +72,7 @@ mkdir -p build/$machine_name-$platform/shared/$target
 pushd build/$machine_name-$platform/shared/$target   
 rm -f path_names                       
 $srcdir/mkmf/bin/list_paths $srcdir/FMS/{affinity,amip_interp,column_diagnostics,diag_integral,drifters,horiz_interp,memutils,sat_vapor_pres,topography,astronomy,constants,diag_manager,field_manager,include,monin_obukhov,platform,tracer_manager,axis_utils,coupler,fms,fms2_io,interpolator,mosaic,mosaic2,random_numbers,time_interp,tridiagonal,block_control,data_override,exchange,mpp,time_manager,string_utils,parser}/ $srcdir/FMS/libFMS.F90
-$srcdir/mkmf/bin/mkmf -t $abs_rootdir/$machine_name/$platform.mk -p libfms.a -c "-Duse_libMPI -Duse_netCDF -DMAXFIELDMETHODS_=800" path_names
+$srcdir/mkmf/bin/mkmf -t $abs_rootdir/$machine_name/$platform.mk -p libfms.a -c "-Duse_deprecated_io -Duse_libMPI -Duse_netCDF -DMAXFIELDMETHODS_=800" path_names
 
 make $makeflags libfms.a         
 
@@ -91,7 +83,7 @@ fi
 
 popd
 
-if [[ $flavor =~ "mom6sis2" ]] ; then
+if [[ $flavor == "mom6sis2" ]] ; then
     mkdir -p build/$machine_name-$platform/ocean_ice/$target
     pushd build/$machine_name-$platform/ocean_ice/$target
     rm -f path_names
@@ -107,19 +99,26 @@ fi
 
     $srcdir/mkmf/bin/mkmf -t $abs_rootdir/$machine_name/$platform.mk -o "-I../../shared/$target" -p MOM6SIS2 -l "-L../../shared/$target -lfms $linker_options" -c "$compiler_options" path_names
 
-if [[ "$target" =~ "cobaltACC" ]] ; then 
-    sed -e 's/-c\(.*\)COBALT/-acc -ta=nvidia:managed -Minfo=accel -c \1COBALT/' -i Makefile
-    sed -e 's/-lfms/-lfms -acc/' -i Makefile
+    make $makeflags MOM6SIS2
+
+elif [[ $flavor == "fms1_mom6sis2" ]]; then
+
+    echo "build mom6sis2 with FMS1 cap" 
+
+    mkdir -p build/$machine_name-$platform/ocean_ice/$target
+    pushd build/$machine_name-$platform/ocean_ice/$target
+    rm -f path_names
+    $srcdir/mkmf/bin/list_paths $srcdir/MOM6/{config_src/infra/FMS1,config_src/memory/dynamic_symmetric,config_src/drivers/FMS_cap,config_src/external/ODA_hooks,config_src/external/database_comms,config_src/external/drifters,config_src/external/stochastic_physics,pkg/GSW-Fortran/{modules,toolbox}/,src/{*,*/*}/} $srcdir/SIS2/{config_src/dynamic_symmetric,config_src/external/Icepack_interfaces,src} $srcdir/icebergs/src $srcdir/FMS/{coupler,include}/ $srcdir/{ocean_BGC/generic_tracers,ocean_BGC/mocsy/src}/ $srcdir/{atmos_null,ice_param,land_null,coupler/shared/,coupler/full/}/
+
+
+compiler_options='-DINTERNAL_FILE_NML -DMAX_FIELDS_=600 -DNOT_SET_AFFINITY -D_USE_MOM6_DIAG -D_USE_GENERIC_TRACER -DUSE_PRECISION=2 -D_USE_LEGACY_LAND_ -Duse_AM3_physics'
+linker_options=''
+if [[ "$target" =~ "stdpar" ]] ; then
+    compiler_options="$compiler_options -stdpar -Minfo=accel"
+    linker_options="$linker_options -stdpar "
 fi
 
-if [[ "$target" =~ "cobaltOMP" ]] ; then 
-    sed -e 's/-c\(.*\)COBALT/-mp -c \1COBALT/' -i Makefile
-fi
-
-if [[ "$target" =~ "cobaltOMPGPU" ]] ; then 
-    sed -e 's/-c\(.*\)COBALT/-mp=gpu -gpu=managed -Minfo=accel -c \1COBALT/' -i Makefile
-    sed -e 's/-lfms/-lfms -mp=gpu -gpu=managed/' -i Makefile
-fi
+    $srcdir/mkmf/bin/mkmf -t $abs_rootdir/$machine_name/$platform.mk -o "-I../../shared/$target" -p MOM6SIS2 -l "-L../../shared/$target -lfms $linker_options" -c "$compiler_options" path_names
 
     make $makeflags MOM6SIS2
 
