4da33be6ded6013796a8edf7c9cf3748f5beb8a5
yuchengt900 <yi-cheng.teng@noaa.gov>
2024-04-30 09:42:51 -0400

    test idea of stable_nightly update

---
+++ .github/workflows/stable_nightly_update.py
@@ -0,0 +1,22 @@
+on:
+#  schedule:
+#    - cron: '0 0 * * *' # Run daily at midnight
+  push:
+    branches:
+      - feature/update-ci
+
+jobs:
+  stable_nightly_update:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Check out repository
+        uses: actions/checkout@v2
+
+      - name: Merge develop into main
+        if: github.ref == 'refs/heads/feature/update-ci'
+        run: |
+          git checkout main_test
+          git pull origin main_test
+          git merge origin/feature/update-ci --no-edit
+          git push origin main_test
