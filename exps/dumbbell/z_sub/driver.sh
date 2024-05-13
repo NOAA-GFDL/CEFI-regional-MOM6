1d799141b6842b04d11d38abbcfde129894c7ef2
Yi-Cheng Teng - NOAA GFDL <143743249+yichengt900@users.noreply.github.com>
2024-05-13 16:45:19 -0400

    Add dumbbell test case (#35)
    * Add dumbbell test case, update build scripti, update container
    
    * missing build process
    
    * fix dumbbell driver
    
    * update mom6 and cobalt hash tags
    
    * fix conflicts
    
    * Remove MOM_layout from input.nml

---
+++ exps/dumbbell/z_sub/driver.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -f ocean.stats ] ; then
+    rm ocean.stats ocean.stats.nc
+fi
+
+#
+echo "First run whole domain case ..."
+pushd ../z
+../../../builds/build/docker-linux-gnu/ocean_ice/debug/MOM6SIS2
+
+echo "Extract and generate BCs for subdomain run"
+bash extract_obc.bash  
+python3 extract_obc.py 
+popd
+
+#
+echo "Run Sub domian"
+../../../builds/build/docker-linux-gnu/ocean_ice/debug/MOM6SIS2
+cat ./ocean.stats
+cat ./ocean.stats.gnu
+diff -q ./ocean.stats ./ocean.stats.gnu > /dev/null || { echo "Error: ocean.stats is not identical, please check! Exiting now..."; exit 1; }
+
+# check err
+ln -fs 00010101.prog.nc prog.nc
+python3 rms_errors.py ../z_sub
+cat ./err.txt
+cat ./err.txt.ref
+diff -q ./err.txt ./err.txt.ref > /dev/null || { echo "Error: subset error is not identical, please check! Exiting now..."; exit 1; }
