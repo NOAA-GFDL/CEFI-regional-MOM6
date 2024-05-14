6818ca004b539979b14313f559a941eb0cbc9ffd
Yi-Cheng Teng - NOAA GFDL <143743249+yichengt900@users.noreply.github.com>
2024-05-14 19:54:35 -0400

    debugging

--- exps/OM4.single_column.COBALT/driver.sh
+++ exps/OM4.single_column.COBALT/driver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #
+export LD_LIBRARY_PATH=../../builds/build/docker-linux-gnu/libyaml/debug/lib:$LD_LIBRARY_PATH
 rm -rf RESTART*
 
 #
@@ -10,6 +11,8 @@ echo "Test started:  " `date`
 echo "run 48hrs test ..."
 ln -fs input.nml_48hr input.nml
 mpirun --allow-run-as-root -np 1 ../../builds/build/docker-linux-gnu/ocean_ice/debug/MOM6SIS2 > out1 2>err1
+tail out1
+tail err1
 mv RESTART RESTART_48hrs
 mv ocean.stats* RESTART_48hrs
 
@@ -17,6 +20,8 @@ mv ocean.stats* RESTART_48hrs
 echo "run 24hrs test ..."
 ln -fs input.nml_24hr input.nml
 mpirun --allow-run-as-root -np 1 ../../builds/build/docker-linux-gnu/ocean_ice/debug/MOM6SIS2 > out2 2>err2
+tail out2
+tail err2
 mv RESTART RESTART_24hrs
 mv ocean.stats* RESTART_24hrs
 
@@ -28,9 +33,10 @@ popd
 
 #
 echo "run 24hrs rst test ..."
-export LD_LIBRARY_PATH=../../builds/build/docker-linux-gnu/libyaml/debug/lib:$LD_LIBRARY_PATH
 ln -fs input.nml_24hr_rst input.nml
 mpirun --allow-run-as-root -np 1 ../../builds/build/docker-linux-gnu/ocean_ice/debug/MOM6SIS2 > out3 2>err3
+tail out3
+tail err3
 mv RESTART RESTART_24hrs_rst
 mv ocean.stats* RESTART_24hrs_rst
 [[ ! -f 20040102.ocean_daily_subset.nc.0000 ]] && echo "Can not find sub-region diag output! Exit!" && exit 1
