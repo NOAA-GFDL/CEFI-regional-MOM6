ffc0b2e076a83fb1c4e4b2f304c65d90435accc7
Utheri Wagura <Utheri.Wagura@an105.princeton.rdhpcs.noaa.gov>
2024-08-05 10:39:49 -0400

    Added config file for diagnostic scripts, modified sst_eval to use config file

--- diagnostics/physics/sst_eval.py
+++ diagnostics/physics/sst_eval.py
@@ -3,7 +3,7 @@ Compare model SST with 1993--2019 data from OISST and GLORYS.
 Uses whatever model data can be found within the directory pp_root,
 and does not try to match the model and observed time periods.
 How to use:
-python sst_eval.py /archive/acr/fre/NWA/2023_04/NWA12_COBALT_2023_04_kpo4-coastatten-physics/gfdl.ncrc5-intel22-prod 
+python sst_eval.py -p /archive/acr/fre/NWA/2023_04/NWA12_COBALT_2023_04_kpo4-coastatten-physics/gfdl.ncrc5-intel22-prod -c config.yaml
 """
 
 import cartopy.crs as ccrs
@@ -17,23 +17,25 @@ import xesmf
 from plot_common import annotate_skill, autoextend_colorbar, corners, get_map_norm, open_var
 
 
-def plot_sst_eval(pp_root):
-    model = open_var(pp_root, 'ocean_monthly', 'tos')
-    model_grid = xarray.open_dataset('../data/geography/ocean_static.nc')
-    target_grid = model_grid[['geolon', 'geolat', 'geolon_c', 'geolat_c']].rename({'geolon': 'lon', 'geolat': 'lat', 'geolon_c': 'lon_b', 'geolat_c': 'lat_b'})
+def plot_sst_eval(pp_root, config):
+    model = open_var(pp_root, config['domain'], 'tos')
+    model_grid = xarray.open_dataset( config['model_grid'] )
+    target_grid = model_grid[config['rename_vars']].rename(config['rename_map'])
     model_ave = model.mean('time').load()
 
-    glorys = xarray.open_dataset('/work/acr/mom6/diagnostics/glorys/glorys_sfc.nc')['thetao'] #.rename({'longitude': 'lon', 'latitude': 'lat'})
-    glorys_lonc, glorys_latc = corners(glorys.lon, glorys.lat)
+    glorys = xarray.open_dataset( config['glorys'] ).squeeze()['thetao'] #.rename({'longitude': 'lon', 'latitude': 'lat'})
+    try:
+        glorys_lonc, glorys_latc = corners(glorys.lon, glorys.lat)
+    except:
+        glorys_lonc, glorys_latc = corners(glorys.longitude, glorys.latitude)
     glorys_ave = glorys.mean('time').load()
     glorys_to_mom = xesmf.Regridder(glorys_ave, target_grid, method='bilinear', unmapped_to_nan=True)
     glorys_rg = glorys_to_mom(glorys_ave)
     delta_glorys = model_ave - glorys_rg
-
     oisst = (
-        xarray.open_mfdataset([f'/work/acr/oisstv2/sst.month.mean.{y}.nc' for y in range(1993, 2020)])
+        xarray.open_mfdataset([config['oisst']+f'sst.month.mean.{y}.nc' for y in range(1993, 2020)])
         .sst
-        .sel(lat=slice(0, 60), lon=slice(360-100, 360-30))
+        .sel(lat=slice(config['lat']['south'], config['lat']['north']), lon=slice(config['lon']['west'] ,config['lon']['east']))
     )
     oisst_lonc, oisst_latc = corners(oisst.lon, oisst.lat)
     oisst_lonc -= 360
@@ -46,7 +48,6 @@ def plot_sst_eval(pp_root):
     oisst_ave = oisst.mean('time').load()
     mom_rg = mom_to_oisst(model_ave)
     delta_oisst = mom_rg - oisst_ave
-
     fig = plt.figure(figsize=(11, 14))
     grid = AxesGrid(fig, 111,
         axes_class=(GeoAxes, dict(projection=ccrs.PlateCarree())),
@@ -60,7 +61,7 @@ def plot_sst_eval(pp_root):
     )
 
     # Discrete levels and colorbar for SST plots
-    levels = np.arange(2, 31, 2)
+    levels = np.arange(config['levels_min'], config['levels_max'], config['levels_step'])
     try:
         import cmcrameri.cm as cmc
     except ModuleNotFoundError:
@@ -70,7 +71,7 @@ def plot_sst_eval(pp_root):
     cmap, norm = get_map_norm(cmap, levels=levels)
 
     # Discrete levels and colorbar for difference plots
-    bias_levels = np.arange(-2, 2.1, 0.25)
+    bias_levels = np.arange(config['bias_min'], config['bias_max'], config['bias_step'])
     bias_cmap, bias_norm = get_map_norm('coolwarm', levels=bias_levels)
     bias_common = dict(cmap=bias_cmap, norm=bias_norm)
 
@@ -104,8 +105,8 @@ def plot_sst_eval(pp_root):
     annotate_skill(model_ave, glorys_rg, grid[5], weights=model_grid.areacello)
 
     for ax in grid:
-        ax.set_xlim(-99, -35)
-        ax.set_ylim(4, 59)
+        ax.set_xlim(config['x']['min'], config['x']['max'])
+        ax.set_ylim(config['y']['min'], config['y']['max'])
         ax.set_xticks([])
         ax.set_yticks([])
         ax.set_xticklabels([])
@@ -113,12 +114,16 @@ def plot_sst_eval(pp_root):
         for s in ax.spines.values():
             s.set_visible(False)
         
-    plt.savefig('figures/sst_eval.png', dpi=300, bbox_inches='tight')
+    plt.savefig(config['figures_dir']+'sst_eval.png', dpi=300, bbox_inches='tight')
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
+    import yaml
     parser = ArgumentParser()
-    parser.add_argument('pp_root', help='Path to postprocessed data (up to but not including /pp/)')
+    parser.add_argument('-p','--pp_root', type=str, help='Path to postprocessed data (up to but not including /pp/)')
+    parser.add_argument('-c','--config', type=str, help='Path to config.yaml file containing relevant paths for diagnostic scripts')
     args = parser.parse_args()
-    plot_sst_eval(args.pp_root)
+    with open(args.config, 'r') as f:
+        config = yaml.safe_load(f)
+    plot_sst_eval(args.pp_root, config)
