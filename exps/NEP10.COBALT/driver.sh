5356f16901da5b4d62aa897ec93a925c96b94048
yuchengt900 <yi-cheng.teng@noaa.gov>
2024-08-13 09:59:13 -0400

    Updated NEP10 CI test driver

--- exps/NEP10.COBALT/driver.sh
+++ exps/NEP10.COBALT/driver.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --nodes=8
+#SBATCH --nodes=5
 #SBATCH --time=60
 #SBATCH --job-name="NEP10.COBALT"
 #SBATCH --output=NEP10.COBALT_o.%j
 #SBATCH --error=NEP10.COBALT_e.%j
 #SBATCH --qos=debug
 #SBATCH --partition=batch
-#SBATCH --clusters=c5
-#SBATCH --account=cefi
+#SBATCH --clusters=c6
+#SBATCH --account=ira-cefi
 
 #
 ntasks1=904
@@ -21,19 +21,19 @@ echo "Test started:  " `date`
 #
 echo "link datasets ..."
 pushd ../
-ln -fs /gpfs/f5/cefi/world-shared/datasets ./
+ln -fs /gpfs/f6/ira-cefi/world-shared/datasets ./
 popd
 
 echo "run 20x56 48hrs test ..."
 ln -fs input.nml_48hr input.nml
-srun --ntasks ${ntasks1} --cpus-per-task=1 --export=ALL ../../builds/build/gaea-ncrc5.intel23/ocean_ice/repro/MOM6SIS2 > out1 2>err1
+srun --ntasks ${ntasks1} --cpus-per-task=1 --export=ALL ../../builds/build/gaea-ncrc6.intel23/ocean_ice/repro/MOM6SIS2 > out1 2>err1
 mv RESTART RESTART_48hrs
 mv ocean.stats RESTART_48hrs
 
 #
 echo "run 20x56 24hrs test ..."
 ln -fs input.nml_24hr input.nml
-srun --ntasks ${ntasks1} --cpus-per-task=1 --export=ALL ../../builds/build/gaea-ncrc5.intel23/ocean_ice/repro/MOM6SIS2 > out2 2>err2
+srun --ntasks ${ntasks1} --cpus-per-task=1 --export=ALL ../../builds/build/gaea-ncrc6.intel23/ocean_ice/repro/MOM6SIS2 > out2 2>err2
 mv RESTART RESTART_24hrs
 mv ocean.stats RESTART_24hrs
 
@@ -46,7 +46,7 @@ popd
 #
 echo "run 20x56 24hrs rst test ..."
 ln -fs input.nml_24hr_rst input.nml
-srun --ntasks ${ntasks1} --cpus-per-task=1 --export=ALL ../../builds/build/gaea-ncrc5.intel23/ocean_ice/repro/MOM6SIS2 > out3 2>err3
+srun --ntasks ${ntasks1} --cpus-per-task=1 --export=ALL ../../builds/build/gaea-ncrc6.intel23/ocean_ice/repro/MOM6SIS2 > out3 2>err3
 mv RESTART RESTART_24hrs_rst
 mv ocean.stats RESTART_24hrs_rst
 
