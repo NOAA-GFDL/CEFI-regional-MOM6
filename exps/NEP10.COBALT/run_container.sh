3cab6ae235f8fab78180c679712f84f0c841b02f
yuchengt900 <yi-cheng.teng@noaa.gov>
2024-11-06 15:15:04 -0500

    add ref and rename stdouts

--- exps/NEP10.COBALT/run_container.sh
+++ exps/NEP10.COBALT/run_container.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodes=5
 #SBATCH --time=60
 #SBATCH --job-name="container_NEP10"
-#SBATCH --output=container_NEP10_o.%j
-#SBATCH --error=container_NEP10_e.%j
+#SBATCH --output=NEP10.COBALT_o.%j
+#SBATCH --error=NEP10.COBALT_e.%j
 #SBATCH --qos=debug
 #SBATCH --partition=batch
 #SBATCH --clusters=c6
@@ -28,6 +28,10 @@ sync && sleep 1
 srun --mpi=pmi2 --ntasks=904 --cpus-per-task=1 --export=ALL ./container_exec/MOM6SIS2
 
 echo "Model ended:    " `date`
+
+#
+diff -q ./ocean.stats ref/docker-linux-intel-repro/ocean.stats > /dev/null || { echo "Error: ocean.stats is not identical to ref, please check! Exiting now..."; exit 1; }
+echo "ocean.stats is identical to ref, PASS"
 
 #
 rm -rf /gpfs/f6/ira-cefi/proj-shared/github/tmp/NEP10/RESTART_container/*
