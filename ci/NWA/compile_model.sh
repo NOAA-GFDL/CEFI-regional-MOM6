3d86eba26449a02b9e45d961d63266a6859b472f
Utheri Wagura <Utheri.Wagura@gaea53.ncrc.gov>
2025-02-28 13:44:54 -0500

    Address comments in first review of  #136

--- ci/NWA/compile_model.sh
+++ ci/NWA/compile_model.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 podman build -f Dockerfile -t mom6_sis2_generic_4p_compile_symm_yaml:prod
-rm -f mom6_sis2_generic_4p_compile_symm_yaml.tar mom6_sis2_generic_4p_compile_symm_yaml.sif
 podman save -o mom6_sis2_generic_4p_compile_symm_yaml-prod.tar localhost/mom6_sis2_generic_4p_compile_symm_yaml:prod
 apptainer build --disable-cache CEFI_NWA12_COBALT_V1.sif docker-archive://mom6_sis2_generic_4p_compile_symm_yaml-prod.tar
