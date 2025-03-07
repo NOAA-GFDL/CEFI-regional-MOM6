548e7be474b03dfb696a786ff2779b219a133031
Yi-Cheng Teng - NOAA GFDL <143743249+yichengt900@users.noreply.github.com>
2025-03-07 15:21:29 -0500

    Weekly update part 2 (#151)
    * updated bgc to latest cefi tag that include fix for oxygen
    
    * Update refs folders

--- exps/NWA12.COBALT/driver.sh
+++ exps/NWA12.COBALT/driver.sh
@@ -78,7 +78,7 @@ mv ocean.stats RESTART_24hrs_rst
 # Define the directories containing the files
 module load nccmp
 DIR1="./RESTART_24hrs_rst"
-DIR2="/gpfs/f6/ira-cefi/proj-shared/github/ci_data/reference/main/NWA12.COBALT/20250131"
+DIR2="/gpfs/f6/ira-cefi/proj-shared/github/ci_data/reference/main/NWA12.COBALT/20250307"
 
 # Define the files to compare
 FILES=("$DIR2"/*.nc)
