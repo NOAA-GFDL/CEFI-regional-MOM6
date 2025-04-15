57f1c2e6f8aeb3a607a7fb5cbb7602a8a0b2d86b
yichengt900 <yi-cheng.teng@noaa.gov>
2025-04-15 15:51:13 -0400

    refactor build script

--- builds/ci_build_driver.sh
+++ builds/ci_build_driver.sh
@@ -6,18 +6,18 @@
 #SBATCH --error=MOM6SIS2_container_build_e.%j
 #SBATCH --qos=urgent
 #SBATCH --partition=batch
-#SBATCH --clusters=c6
-#SBATCH --account=ira-cefi
+#SBATCH --clusters=c5
+#SBATCH --account=cefi
 
 #
 [ -d "build" ] && rm -rf build
 
 #
-echo "Build MOM6SIS2-COBALT using container started:  " `date`
+echo "Build MOM6 using container started:  " `date`
 
 #
-export img=/gpfs/f6/ira-cefi/world-shared/container/cefi_mom6_intel_2024.2.1.sif
-apptainer exec -B /gpfs -B /ncrc/home2/Yi-cheng.Teng:/ncrc/home2/Yi-cheng.Teng $img bash linux-build.bash -m docker -p linux-intel -t repro -f mom6sis2
+export img=/gpfs/f5/cefi/world-shared/container/cefi_mom6_intel_2024.2.1.sif
+apptainer exec -B /gpfs -B /ncrc/home2/Yi-cheng.Teng:/ncrc/home2/Yi-cheng.Teng $img bash linux-build.bash -m docker -p linux-intel -t repro -f mom6solo
 
 #
-echo "Build MOM6SIS2-COBALT using container ended:  " `date`
+echo "Build MOM6 using container ended:  " `date`
