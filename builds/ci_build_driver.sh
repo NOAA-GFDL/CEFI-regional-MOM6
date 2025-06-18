b7ed98340d892ee47bc2d88199bc41562a668863
yichengt900 <yi-cheng.teng@noaa.gov>
2025-06-18 12:32:09 -0400

    change to debug

--- builds/ci_build_driver.sh
+++ builds/ci_build_driver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=120
+#SBATCH --time=60
 #SBATCH --job-name="MOM6SIS2_container_build"
 #SBATCH --output=MOM6SIS2_container_build_o.%j
 #SBATCH --error=MOM6SIS2_container_build_e.%j
-#SBATCH --qos=normal
+#SBATCH --qos=debug
 #SBATCH --partition=batch
 #SBATCH --clusters=c5
 #SBATCH --account=gfdl_med
