33e32723163443d4a1fed9f8415c7e1fb0abb0ed
yichengt900 <yi-cheng.teng@noaa.gov>
2025-06-18 12:10:15 -0400

    enhanced ci build script

--- builds/ci_build_driver.sh
+++ builds/ci_build_driver.sh
@@ -20,7 +20,20 @@ echo "Build MOM6SIS2-COBALT using container started:  " `date`
 #apptainer exec -B /gpfs -B /ncrc/home2/Yi-cheng.Teng:/ncrc/home2/Yi-cheng.Teng $img bash linux-build.bash -m docker -p linux-intel -t repro -f mom6sis2
 
 export img=/gpfs/f5/cefi/world-shared/container/gaea_intel_2023.sif
-apptainer exec -B /gpfs -B /ncrc/home2/$USER:/ncrc/home2/$USER -B /autofs/ncrc-svm1_home1/role.medgrp:/autofs/ncrc-svm1_home1/role.medgrp $img bash linux-build.bash -m docker -p linux-intel -t repro -f mom6sis2
+
+if [ "$USER" = "role.medgrp" ]; then
+    apptainer exec \
+        -B /gpfs \
+        -B /autofs/ncrc-svm1_home1/role.medgrp:/autofs/ncrc-svm1_home1/role.medgrp \
+        "$img" \
+        bash linux-build.bash -m docker -p linux-intel -t repro -f mom6sis2
+else
+    apptainer exec \
+        -B /gpfs \
+        -B /ncrc/home2/$USER:/ncrc/home2/$USER \
+        "$img" \
+        bash linux-build.bash -m docker -p linux-intel -t repro -f mom6sis2
+fi
 
 #
 echo "Build MOM6SIS2-COBALT using container ended:  " `date`
