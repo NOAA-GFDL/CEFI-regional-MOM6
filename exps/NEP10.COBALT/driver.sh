70644e5ab5bec5e7c1e679c967e436a3845654bf
yichengt900 <yi-cheng.teng@noaa.gov>
2025-06-17 21:07:09 -0400

    Updated reference date

--- exps/NEP10.COBALT/driver.sh
+++ exps/NEP10.COBALT/driver.sh
@@ -100,7 +100,7 @@ mv ocean.stats RESTART_24hrs_rst
 # Define the directories containing the files
 module load nccmp
 DIR1="./RESTART_24hrs_rst"
-DIR2="/gpfs/f6/ira-cefi/proj-shared/github/ci_data/reference/main/NEP10.COBALT/20250613" 
+DIR2="/gpfs/f6/ira-cefi/proj-shared/github/ci_data/reference/main/NEP10.COBALT/20250616" 
 
 # Define the files to compare
 FILES=("$DIR2"/MOM.res*.nc)
