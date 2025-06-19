b3b48bc918abc8ad916faf4f6fe63c08da0d64d3
Yi-Cheng Teng - NOAA GFDL <143743249+yichengt900@users.noreply.github.com>
2025-06-19 15:05:30 -0400

    Changed RT from c6 to c5 using role account (#207)
    * Changed RT from c6 to c5 using role account
    
    * Forget update ci build script
    
    * fixed ci build script
    
    * enhanced ci build script
    
    * change to debug
    
    * change ci build script again
    
    * tried both home folder pathes
    
    * updated driver.sh to remove libcxi
    
    * Updated driver.sh
    
    * force gha run in bash shell
    
    * added bash env in driver.sh
    
    * updaye nwa run script
    
    * Tried updated driver and field_table
    
    * Updated nep field_table and driver
    
    * Update parameter documentation files for NWA12.COBALT

--- exps/NWA12.COBALT/driver.sh
+++ exps/NWA12.COBALT/driver.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --nodes=9
+#SBATCH --nodes=13
 #SBATCH --time=120
 #SBATCH --job-name="NWA12.COBALT"
 #SBATCH --output=NWA12.COBALT_o.%j
 #SBATCH --error=NWA12.COBALT_e.%j
-#SBATCH --qos=urgent
+#SBATCH --qos=normal
 #SBATCH --partition=batch
-#SBATCH --clusters=c6
-#SBATCH --account=ira-cefi
+#SBATCH --clusters=c5
+#SBATCH --account=gfdl_med
 
 # Default to not using shared project folders
 USE_PROJ_SHARED=false
@@ -28,17 +28,17 @@ ntasks2=900
 
 #
 echo "Test started:  " `date`
-
+source $MODULESHOME/init/bash
 module load cray-mpich-abi
 module unload cray-hdf5
 
 #
 echo "link datasets ..."
 pushd ../
-ln -fs /gpfs/f6/ira-cefi/world-shared/datasets ./
+ln -fs /gpfs/f5/gfdl_med/world-shared/datasets ./
 popd
 
-export img="/gpfs/f6/ira-cefi/world-shared/container/gaea_intel_2023.sif"
+export img="/gpfs/f5/cefi/world-shared/container/gaea_intel_2023.sif"
 
 echo "SET MPICH_SMP_SINGLE_COPY_MODE"
 export MPICH_SMP_SINGLE_COPY_MODE="NONE"
@@ -46,7 +46,8 @@ export MPICH_SMP_SINGLE_COPY_MODE="NONE"
 export APPTAINERENV_LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}:/opt/cray/pe/lib64:/usr/lib64/libibverbs:/opt/cray/libfabric/1.20.1/lib64:/opt/cray/pals/1.4/lib:\$LD_LIBRARY_PATH
 
 echo "SET APPTAINER_CONTAINLIBS"
-export APPTAINER_CONTAINLIBS="/usr/lib64/libcxi.so,/usr/lib64/libcxi.so.1,/usr/lib64/libcxi.so.1.5.0,/usr/lib64/libjansson.so.4,/usr/lib64/libjson-c.so.3,/usr/lib64/libdrm.so.2,/lib64/libtinfo.so.6,/usr/lib64/libnl-3.so.200,/usr/lib64/librdmacm.so.1,/usr/lib64/libibverbs.so.1,/usr/lib64/libibverbs/libmlx5-rdmav34.so,/usr/lib64/libnuma.so.1,/usr/lib64/libnl-cli-3.so.200,/usr/lib64/libnl-genl-3.so.200,/usr/lib64/libnl-nf-3.so.200,/usr/lib64/libnl-route-3.so.200,/usr/lib64/libnl-3.so.200,/usr/lib64/libnl-idiag-3.so.200,/usr/lib64/libnl-xfrm-3.so.200,/usr/lib64/libnl-genl-3.so.200"
+export APPTAINER_CONTAINLIBS="/usr/lib64/libjansson.so.4,/usr/lib64/libjson-c.so.3,/usr/lib64/libdrm.so.2,/lib64/libtinfo.so.6,/usr/lib64/libnl-3.so.200,/usr/lib64/librdmacm.so.1,/usr/lib64/libibverbs.so.1,/usr/lib64/libibverbs/libmlx5-rdmav34.so,/usr/lib64/libnuma.so.1,/usr/lib64/libnl-cli-3.so.200,/usr/lib64/libnl-genl-3.so.200,/usr/lib64/libnl-nf-3.so.200,/usr/lib64/libnl-route-3.so.200,/usr/lib64/libnl-3.so.200,/usr/lib64/libnl-idiag-3.so.200,/usr/lib64/libnl-xfrm-3.so.200,/usr/lib64/libnl-genl-3.so.200"
+#export APPTAINER_CONTAINLIBS="/usr/lib64/libcxi.so,/usr/lib64/libcxi.so.1,/usr/lib64/libcxi.so.1.5.0,/usr/lib64/libjansson.so.4,/usr/lib64/libjson-c.so.3,/usr/lib64/libdrm.so.2,/lib64/libtinfo.so.6,/usr/lib64/libnl-3.so.200,/usr/lib64/librdmacm.so.1,/usr/lib64/libibverbs.so.1,/usr/lib64/libibverbs/libmlx5-rdmav34.so,/usr/lib64/libnuma.so.1,/usr/lib64/libnl-cli-3.so.200,/usr/lib64/libnl-genl-3.so.200,/usr/lib64/libnl-nf-3.so.200,/usr/lib64/libnl-route-3.so.200,/usr/lib64/libnl-3.so.200,/usr/lib64/libnl-idiag-3.so.200,/usr/lib64/libnl-xfrm-3.so.200,/usr/lib64/libnl-genl-3.so.200"
 
 echo "SET APPTAINER_BIND"
 export APPTAINER_BIND="/usr/share/libdrm,/var/spool/slurmd,/opt/cray,/opt/intel,${PWD},/etc/libibverbs.d,/usr/lib64/libibverbs,/usr/lib64/libnl3-200,${HOME}"
@@ -56,9 +57,9 @@ export FI_VERBS_PREFER_XRC=0
 #
 if $USE_PROJ_SHARED; then
   echo "clean RESTART folders ..."
-  rm -rf /gpfs/f6/ira-cefi/proj-shared/github/tmp/NWA12/RESTART_48hrs/*
-  rm -rf /gpfs/f6/ira-cefi/proj-shared/github/tmp/NWA12/RESTART_24hrs/*
-  rm -rf /gpfs/f6/ira-cefi/proj-shared/github/tmp/NWA12/RESTART_24hrs_rst/*
+  rm -rf /gpfs/f5/gfdl_med/proj-shared/github/tmp/NWA12/RESTART_48hrs/*
+  rm -rf /gpfs/f5/gfdl_med/proj-shared/github/tmp/NWA12/RESTART_24hrs/*
+  rm -rf /gpfs/f5/gfdl_med/proj-shared/github/tmp/NWA12/RESTART_24hrs_rst/*
 fi
 
 echo "run 40x40 48hrs test ..."
@@ -68,9 +69,9 @@ ln -fs MOM_layout_40 MOM_layout
 ln -fs MOM_layout_40 SIS_layout
 popd
 if $USE_PROJ_SHARED; then
-  ln -fs /gpfs/f6/ira-cefi/proj-shared/github/tmp/NWA12/RESTART_48hrs ./RESTART
+  ln -fs /gpfs/f5/gfdl_med/proj-shared/github/tmp/NWA12/RESTART_48hrs ./RESTART
 fi
-srun --ntasks ${ntasks1} --export=ALL apptainer exec --writable-tmpfs $img ./execrunscript.sh > out1 2>err1
+srun --ntasks ${ntasks1} --export=ALL apptainer exec -B $HOME:$HOME -B /autofs/ncrc-svm1_home1/role.medgrp:/autofs/ncrc-svm1_home1/role.medgrp  --writable-tmpfs $img bash ./execrunscript.sh > out1 2>err1
 mv RESTART RESTART_48hrs
 mv ocean.stats RESTART_48hrs
 
@@ -78,9 +79,9 @@ mv ocean.stats RESTART_48hrs
 echo "run 40x40 24hrs test ..."
 ln -fs input.nml_24hr input.nml
 if $USE_PROJ_SHARED; then
-  ln -fs /gpfs/f6/ira-cefi/proj-shared/github/tmp/NWA12/RESTART_24hrs ./RESTART
+  ln -fs /gpfs/f5/gfdl_med/proj-shared/github/tmp/NWA12/RESTART_24hrs ./RESTART
 fi
-srun --ntasks ${ntasks1} --export=ALL apptainer exec --writable-tmpfs $img ./execrunscript.sh > out2 2>err2
+srun --ntasks ${ntasks1} --export=ALL apptainer exec -B $HOME:$HOME -B /autofs/ncrc-svm1_home1/role.medgrp:/autofs/ncrc-svm1_home1/role.medgrp --writable-tmpfs $img bash ./execrunscript.sh > out2 2>err2
 mv RESTART RESTART_24hrs
 mv ocean.stats RESTART_24hrs
 
@@ -98,9 +99,9 @@ ln -fs MOM_layout_30 MOM_layout
 ln -fs MOM_layout_30 SIS_layout
 popd
 if $USE_PROJ_SHARED; then
-  ln -fs /gpfs/f6/ira-cefi/proj-shared/github/tmp/NWA12/RESTART_24hrs_rst ./RESTART
+  ln -fs /gpfs/f5/gfdl_med/proj-shared/github/tmp/NWA12/RESTART_24hrs_rst ./RESTART
 fi
-srun --ntasks ${ntasks2} --export=ALL apptainer exec --writable-tmpfs $img ./execrunscript.sh > out3 2>err3
+srun --ntasks ${ntasks2} --export=ALL apptainer exec -B $HOME:$HOME -B /autofs/ncrc-svm1_home1/role.medgrp:/autofs/ncrc-svm1_home1/role.medgrp --writable-tmpfs $img bash ./execrunscript.sh > out3 2>err3
 mv RESTART RESTART_24hrs_rst
 mv ocean.stats RESTART_24hrs_rst
 
@@ -108,7 +109,7 @@ mv ocean.stats RESTART_24hrs_rst
 # Define the directories containing the files
 module load nccmp
 DIR1="./RESTART_24hrs_rst"
-DIR2="/gpfs/f6/ira-cefi/proj-shared/github/ci_data/reference/main/NWA12.COBALT/20250616"
+DIR2="/gpfs/f5/gfdl_med/proj-shared/github/ci_data/reference/main/NWA12.COBALT/20250616"
 
 # Define the files to compare
 FILES=("$DIR2"/*.nc)
@@ -127,9 +128,9 @@ echo "All restart files are identical, PASS"
 #
 if $USE_PROJ_SHARED; then
   echo "clean RESTART folders now ..."
-  rm -rf /gpfs/f6/ira-cefi/proj-shared/github/tmp/NWA12/RESTART_48hrs/*
-  rm -rf /gpfs/f6/ira-cefi/proj-shared/github/tmp/NWA12/RESTART_24hrs/*
-  rm -rf /gpfs/f6/ira-cefi/proj-shared/github/tmp/NWA12/RESTART_24hrs_rst/*
+  rm -rf /gpfs/f5/gfdl_med/proj-shared/github/tmp/NWA12/RESTART_48hrs/*
+  rm -rf /gpfs/f5/gfdl_med/proj-shared/github/tmp/NWA12/RESTART_24hrs/*
+  rm -rf /gpfs/f5/gfdl_med/proj-shared/github/tmp/NWA12/RESTART_24hrs_rst/*
 fi
 
 echo "Test ended:  " `date`
