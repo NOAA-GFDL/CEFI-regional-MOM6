0ec4ecbfd151bc79c6e5104da8519f4fc90e10d6
Andrew Ross <5852283+andrew-c-ross@users.noreply.github.com>
2026-03-10 11:53:52 -0400

    Add tests for o2, no3, alk budget closure in 1D case
    Expected to fail budget check at surface because vertfill_post is currently enabled. Will disable in a subsequent commit

--- exps/OM4.single_column.COBALT/driver.sh
+++ exps/OM4.single_column.COBALT/driver.sh
@@ -8,15 +8,6 @@ rm -rf RESTART*
 echo "Test started:  " `date`
 
 #
-echo "run 48hrs test ..."
-ln -fs input.nml_48hr input.nml
-mpirun --allow-run-as-root -np 1 ../../builds/build/docker-linux-gnu/ocean_ice/debug/MOM6SIS2 > out1 2>err1
-tail out1
-tail err1
-mv RESTART RESTART_48hrs
-mv ocean.stats* RESTART_48hrs
-
-#
 echo "run 24hrs test ..."
 ln -fs input.nml_24hr input.nml
 mpirun --allow-run-as-root -np 1 ../../builds/build/docker-linux-gnu/ocean_ice/debug/MOM6SIS2 > out2 2>err2
@@ -40,6 +31,18 @@ tail err3
 mv RESTART RESTART_24hrs_rst
 mv ocean.stats* RESTART_24hrs_rst
 [[ ! -f 20040102.ocean_daily_subset.nc.0000 ]] && echo "Can not find sub-region diag output! Exit!" && exit 1
+
+# Run the continuous 48 hour experiment last, because this one will save
+# extra diagnostics for the BGC budget test
+echo "run 48hrs test ..."
+ln -fs input.nml_48hr input.nml
+# Append extra BGC budget diagnostics
+cat diag_table_budget >> diag_table
+mpirun --allow-run-as-root -np 1 ../../builds/build/docker-linux-gnu/ocean_ice/debug/MOM6SIS2 > out1 2>err1
+tail out1
+tail err1
+mv RESTART RESTART_48hrs
+mv ocean.stats* RESTART_48hrs
 
 # Define the directories containing the files
 DIR1="RESTART_24hrs_rst/"
