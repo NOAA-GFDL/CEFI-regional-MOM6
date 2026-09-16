19adbf7af17244e8adaffdc4d1cdc30668e57570
Utheri Wagura <Utheri.Wagura@noaa.gov>
2026-09-16 16:20:28 -0400

    Update ci_build_driver for c6, ifx baremetal testing

--- builds/ci_build_driver.sh
+++ builds/ci_build_driver.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=60
-#SBATCH --job-name="MOM6SIS2_container_build"
-#SBATCH --output=MOM6SIS2_container_build_o.%j
-#SBATCH --error=MOM6SIS2_container_build_e.%j
+#SBATCH --job-name="MOM6SIS2_c6_baremetal_ifx_build"
+#SBATCH --output=MOM6SIS2_c6_baremetal_ifx_build_o.%j
 #SBATCH --qos=debug
 #SBATCH --partition=batch
-#SBATCH --clusters=c5
-#SBATCH --account=gfdl_med
+#SBATCH --clusters=c6
+#SBATCH --account=ira-cefi
 
 #
 [ -d "build" ] && rm -rf build
@@ -15,26 +14,6 @@
 #
 echo "Build MOM6SIS2-COBALT using container started:  " `date`
 
-#
-#export img=/gpfs/f6/ira-cefi/world-shared/container/cefi_mom6_intel_2024.2.1.sif
-#apptainer exec -B /gpfs -B /ncrc/home2/Yi-cheng.Teng:/ncrc/home2/Yi-cheng.Teng $img bash linux-build.bash -m docker -p linux-intel -t repro -f mom6sis2
+bash linux-build.bash
 
-export img=/gpfs/f5/cefi/world-shared/container/gaea_intel_2023.2.0.sif
-
-if [ "$USER" = "role.medgrp" ]; then
-    apptainer exec \
-        -B /gpfs \
-	-B /autofs/ncrc-svm1_home1/role.medgrp:/autofs/ncrc-svm1_home1/role.medgrp \
-        -B $HOME:$HOME \
-        "$img" \
-        bash linux-build.bash -m docker -p linux-intel -t repro -f mom6sis2
-else
-    apptainer exec \
-        -B /gpfs \
-        -B $HOME:$HOME \
-        "$img" \
-        bash linux-build.bash -m docker -p linux-intel -t repro -f mom6sis2
-fi
-
-#
 echo "Build MOM6SIS2-COBALT using container ended:  " `date`
